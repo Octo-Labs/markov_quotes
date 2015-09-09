7c0021beaded1116eb287c91b06ead68fd5941f2
Jeremy Green <jeremy@octolabs.com>
2015-09-08 21:26:59 -0500

    The basic naive implementation is working

--- lib/markov_quotes/markov_chainer.rb
+++ lib/markov_quotes/markov_chainer.rb
@@ -3,12 +3,19 @@ module MarkovQuotes
 
     def initialize
       @beginnings = []
+      @words = {}
     end
 
     def train(sentence)
       previous_word = nil
       sentence.split.reverse.each do |w|
-        word = Word.new(w,previous_word)
+        if @words[w]
+          word = @words[w]
+          word.add_next_word(previous_word)
+        else
+          word = Word.new(w,previous_word)
+          @words[w] = word
+        end
         previous_word = word
       end
       @beginnings.push previous_word
@@ -16,7 +23,7 @@ module MarkovQuotes
 
     def generate
       words = []
-      word = @beginnings.first
+      word = @beginnings.sample
       while(word)
         words.push(word)
         word = word.next_word
--- lib/markov_quotes/word.rb
+++ lib/markov_quotes/word.rb
@@ -2,11 +2,19 @@ module MarkovQuotes
   class Word
 
     attr_reader :word
-    attr_reader :next_word
+    attr_reader :next_words
 
     def initialize(word, next_word)
       @word = word
-      @next_word = next_word
+      @next_words = [next_word]
+    end
+
+    def add_next_word(next_word)
+      @next_words.push(next_word)
+    end
+
+    def next_word
+      @next_words.sample
     end
 
     def to_s
